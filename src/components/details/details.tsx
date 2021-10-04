79a9e97a5f6ee83a3ec5db0a6eabca2be090e588
SRINIVAS-B-SINDAGI <srinivas.sindagi@hebbalelabs.com>
2021-10-04 15:03:12 +0530

    Implementing back button * show loading state in detials screen

--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -1,10 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Container, Grid } from "@mui/material";
+import { CircularProgress, Container, Grid } from "@mui/material";
 import axios from "axios";
 import React from "react";
 import { serverUrl } from "../../App";
 import ReactApexChart from "react-apexcharts";
 import { calculateDate } from "../../utils/utils";
+import { useHistory } from "react-router-dom";
 
 export default function Details(props: {
   match: { params: { filename: string; securityname: string } };
@@ -12,13 +13,14 @@ export default function Details(props: {
   const { user, getAccessTokenSilently } = useAuth0();
   const [requestToken, setRequestToken] = React.useState<string | undefined>();
   const [sdValue, setSdValue] = React.useState<string>("1M");
+  const [loading, setLoadingData] = React.useState<string>("loading");
   const [graphData, setGraphData] = React.useState({
     date: [],
     open: [],
     close: [],
     volume: [],
   });
-
+  const history = useHistory();
   React.useEffect(() => {
     let end = new Date();
     let start = new Date();
@@ -75,14 +77,16 @@ export default function Details(props: {
         });
         console.debug(temp);
         setGraphData(temp);
+        setLoadingData("loaded");
       })
       .catch(function (error) {
         console.debug(error);
       });
   };
 
+  console.log(loading);
   const handleChange = (ds: any) => {
-    console.log(ds);
+    setLoadingData("loading");
     setSdValue(ds);
     const { end, start } = calculateDate(ds);
     getStandardDeviation(requestToken, props.match.params.filename, start, end);
@@ -198,84 +202,102 @@ export default function Details(props: {
     },
   };
 
+  const handleButtonClick = () => {
+    history.push({
+      pathname: `/`,
+    });
+  };
   return (
     <React.Fragment>
-      <Container fixed>
-        <React.Fragment>
-          <div style={{ margin: 40 }}>
-            <button
-              style={{
-                border: "none",
-                borderRadius: 4,
-                height: 32,
-                width: 60,
-                cursor: "pointer",
-                backgroundColor: "#2E3B55",
-                color: "white",
-                fontWeight: 600,
-              }}
-            >
-              BACK
-            </button>
-            <p style={{ fontSize: 20 }}>
-              {JSON.parse(localStorage.getItem("company_name")!)}
-            </p>
-            <hr />
-            <div style={{ float: "right", padding: 12 }}>
-              {sdValues.map((sd: string, index) => (
-                <button
-                  onClick={(event) => handleChange(sd)}
-                  key={sd}
-                  style={{
-                    border: "none",
-                    borderRadius: 4,
-                    height: 32,
-                    width: 40,
-                    cursor: "pointer",
-                    backgroundColor: "#2E3B55",
-                    color: "white",
-                    marginLeft: 10,
-                    fontWeight: 600,
-                  }}
-                >
-                  {sd}
-                </button>
-              ))}
-            </div>
-            <div>
-              <p>
-                Selected Standard Deviation : <b>{sdValue}</b>
+      {loading === "loading" ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: 100,
+          }}
+        >
+          <CircularProgress />
+        </div>
+      ) : (
+        <Container fixed>
+          <React.Fragment>
+            <div style={{ margin: 40 }}>
+              <button
+                onClick={(event) => handleButtonClick()}
+                style={{
+                  border: "none",
+                  borderRadius: 4,
+                  height: 32,
+                  width: 60,
+                  cursor: "pointer",
+                  backgroundColor: "#2E3B55",
+                  color: "white",
+                  fontWeight: 600,
+                }}
+              >
+                BACK
+              </button>
+              <p style={{ fontSize: 20 }}>
+                {JSON.parse(localStorage.getItem("company_name")!)}
               </p>
+              <hr />
+              <div style={{ float: "right", padding: 12 }}>
+                {sdValues.map((sd: string, index) => (
+                  <button
+                    onClick={(event) => handleChange(sd)}
+                    key={sd}
+                    style={{
+                      border: "none",
+                      borderRadius: 4,
+                      height: 32,
+                      width: 40,
+                      cursor: "pointer",
+                      backgroundColor: "#2E3B55",
+                      color: "white",
+                      marginLeft: 10,
+                      fontWeight: 600,
+                    }}
+                  >
+                    {sd}
+                  </button>
+                ))}
+              </div>
+              <div>
+                <p>
+                  Selected Standard Deviation : <b>{sdValue}</b>
+                </p>
+              </div>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={12} md={6} lg={6}>
+                  <ReactApexChart
+                    options={openLine.options}
+                    series={openLine.series}
+                    type="area"
+                    height={350}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={12} md={6} lg={6}>
+                  <ReactApexChart
+                    options={closeLine.options}
+                    series={closeLine.series}
+                    type="area"
+                    height={350}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={12} md={6} lg={6}>
+                  <ReactApexChart
+                    options={BarState.options}
+                    series={BarState.series}
+                    type="line"
+                    height={350}
+                  />
+                </Grid>
+              </Grid>
             </div>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={12} md={6} lg={6}>
-                <ReactApexChart
-                  options={openLine.options}
-                  series={openLine.series}
-                  type="area"
-                  height={350}
-                />
-              </Grid>
-              <Grid item xs={12} sm={12} md={6} lg={6}>
-                <ReactApexChart
-                  options={closeLine.options}
-                  series={closeLine.series}
-                  type="area"
-                  height={350}
-                />
-              </Grid>
-              <Grid item xs={12} sm={12} md={6} lg={6}>
-                <ReactApexChart
-                  options={BarState.options}
-                  series={BarState.series}
-                  type="line"
-                  height={350}
-                />
-              </Grid>
-            </Grid>
-          </div>
-        </React.Fragment>
-      </Container>
+          </React.Fragment>
+        </Container>
+      )}
     </React.Fragment>
   );
 }
